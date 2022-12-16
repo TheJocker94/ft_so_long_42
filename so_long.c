d9987a98491b53f4487926b8d2cc616ba83acf65
dani <danielecastellano94@gmail.com>
2022-12-17 00:41:03 +0100

    lib aggiunta e tile struct

--- so_long.c
+++ so_long.c
@@ -1,18 +1,5 @@
-#include "minilibx-linux/mlx.h"
-
-typedef struct struct_mlx
-{
-	void	*mlx;
-	void	*mlx_win;
-}mlx;
-
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int	bits_per_pixel;
-	int	line_length;
-	int	endian;
-}t_data;
+//#include "minilibx-linux/mlx.h"
+#include "lib/so_long.h"
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
